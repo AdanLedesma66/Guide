53b7a0bf66e9bbe96b412048988447b3731cf63c
aledesma <adagalese@gmail.com>
2023-06-26 21:43:07 -0400

    service

--- src/main/java/py/com/adan/guide/service/CoursesService.java
+++ src/main/java/py/com/adan/guide/service/CoursesService.java
@@ -2,12 +2,27 @@ package py.com.adan.guide.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import py.com.adan.guide.entity.Courses;
 import py.com.adan.guide.repository.CoursesRepository;
+
+import java.util.List;
+
 @Service
 public class CoursesService {
 
   @Autowired
   private CoursesRepository coursesRepository;
 
+  public List<Courses> findAll(){
+    return coursesRepository.findAll();
+  }
+
+  public Courses findBy(Long id){
+    return coursesRepository.findById(id).orElse(null);
+  }
+
+  public Courses save(Courses courses){
+    return coursesRepository.save(courses);
+  }
 
 }
--- src/main/java/py/com/adan/guide/service/ProyectService.java
+++ src/main/java/py/com/adan/guide/service/ProyectService.java
@@ -2,11 +2,25 @@ package py.com.adan.guide.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import py.com.adan.guide.entity.Proyects;
+import py.com.adan.guide.repository.ProyectsRepository;
+
+import java.util.List;
 
 @Service
 public class ProyectService {
 
   @Autowired
-  private ProyectService proyectService;
+  private ProyectsRepository proyectsRepository;
 
+  public List<Proyects> findAll(){
+    return proyectsRepository.findAll();
+  }
+
+  public Proyects findById(Long id){
+    return proyectsRepository.findById(id).orElse(null);
+  }
+  public Proyects save(Proyects proyects){
+    return proyectsRepository.save(proyects);
+  }
 }
